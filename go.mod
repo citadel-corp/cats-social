3fe12682c2ddf7a7bcb250712dc7ea5a9bf94191
Anindito Bhagawanta <anindito.87bagas@gmail.com>
2024-04-30 19:26:53 +0700

    chore: rename module

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/citadel-corp/go-project-template
+module github.com/citadel-corp/cats-social
 
 go 1.22.0
 
--- internal/user/service.go
+++ internal/user/service.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/citadel-corp/go-project-template/internal/common/jwt"
-	"github.com/citadel-corp/go-project-template/internal/common/password"
+	"github.com/citadel-corp/cats-social/internal/common/jwt"
+	"github.com/citadel-corp/cats-social/internal/common/password"
 )
 
 type Service interface {
