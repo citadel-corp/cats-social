bf00ebcf3605c953fdfb53a0f215278c9b6e43b4
Farolina Rahmatunnisa <rfarolina@gmail.com>
2024-05-05 08:27:48 +0700

    fix: change to port 8080

--- cmd/main.go
+++ cmd/main.go
@@ -98,7 +98,7 @@ func main() {
 	cr.HandleFunc("/{id}", middleware.Authorized(catHandler.DeleteCat)).Methods(http.MethodDelete)
 
 	httpServer := &http.Server{
-		Addr:     ":8000",
+		Addr:     ":8080",
 		Handler:  r,
 		ErrorLog: slog.NewLogLogger(slogHandler, slog.LevelError),
 	}
